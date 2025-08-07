537610eeaf2d375895650c8be60e3c17e229dc42
dohyun_lee <ehtm01@naver.com>
2025-08-08 00:23:32 +0900

    0808 hw

--- SWEA/List_02/16312.py
+++ SWEA/List_02/16312.py
@@ -1,23 +1,35 @@
 # 16312. 2일차 - 이진탐색
 # https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZhdubEaIe3HBIT9&contestProbId=AYYqKXCKE6gDFAVw&probBoxId=AZiB15-a_6bHBIT9&type=USER&problemBoxTitle=0807_List2&problemBoxCnt=6
 
+
+def binarySearch(P, key):
+    start = 1
+    end = P
+    cnt = 0
+    while start <= end:
+        cnt += 1
+        middle = int((start + end) / 2)
+        if middle == key:
+            return cnt
+        elif middle > key:
+            end = middle
+        else:
+            start = middle
+
+    return cnt
+
+
 T = int(input())
 
 for tc in range(1, T+1):
     P, Pa, Pb = map(int, input().split())
-    book_lst = list(map(int, range(P)))
-    start = 0
-    end = P - 1
-    result = []
+    cnt_a = binarySearch(P, Pa)
+    cnt_b = binarySearch(P, Pb)
 
-    while start <= end:
-        middle = (start + end) // 2
-        if book_lst[middle] == Pa:
-            result.append(book_lst[middle])
-        elif book_lst[middle] > Pa:
-            end = middle - 1
-        else:
-            start = middle + 1
-
-
-
+    if cnt_a < cnt_b:
+        winner = 'A'
+    elif cnt_b < cnt_a:
+        winner = 'B'
+    else:
+        winner = '0'
+    print(f'#{tc} {winner}')
