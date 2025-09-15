074331c0f5e4ea987c447e3679a676aa32562d6b
dohyun <ehtm01@naver.com>
2025-09-15 17:52:32 +0900

    0915_write

---
+++ BAEKJOON/Silver/BAEK_9358_순열_접기_게임.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+
+def fold(length, array):
+    if length == 2:
+        return
+
+    ceil = length // 2
+    a = []
+    for i in range(length - 1, ceil, -1):
+
+
+
+
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    fold(N, arr)
