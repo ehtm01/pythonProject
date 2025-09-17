532ea372ab4464a069c0eb8e1edf46e274dbdef6
dohyun <ehtm01@gmail.com>
2025-09-17 17:54:00 +0900

    0917_study

--- BAEKJOON/Gold/BAEK_12865_평범한_배낭.py
+++ BAEKJOON/Gold/BAEK_12865_평범한_배낭.py
@@ -2,19 +2,13 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-
-def backpack(idx, w, v):
-    if w > K:
-        return v
-    dp[idx] += backpack(idx, w + arr[idx][0], v + arr[idx][1])
-
 N, K = map(int, input().split())
-arr = []
-for _ in range(N):
+dp = [0] * (K + 1)  # 가치 저장할 dp 1차원 배열 생성
+for i in range(N):
+    # 무게와 가치를 반복하여 입력받음
     W, V = map(int, input().split())
-    arr.append((W, V))
+    #
+    for w in range(K, W - 1, -1):
+        dp[w] = max(dp[w], dp[w - W] + V)
 
-arr.sort()
-dp = [0] * N
-best = 0
-backpack(0)
+print(max(dp))
--- BAEKJOON/Gold/BAEK_17406_배열_돌리기_4.py
+++ BAEKJOON/Gold/BAEK_17406_배열_돌리기_4.py
@@ -1,33 +1,60 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+from copy import deepcopy
+
+
+# 배열 회전시켜야 함
+# temp값 하나 잡고 나머지 4면으로 돌리자
+# 마지막에 temp값 넣어주면 끝
+def spin(row, col, step, matrix):
+    temp = matrix[row][col + step]
+    for j in range(step, 0, -1):
+        matrix[row][col + j] = matrix[row][col + j - 1]
+    for i in range(step):
+        matrix[row + i][col] = matrix[row + i + 1][col]
+    for j in range(step):
+        matrix[row + step][col + j] = matrix[row + step][col + j + 1]
+    for i in range(step, 1, -1):
+        matrix[row + i][col + step] = matrix[row + i - 1][col + step]
+    matrix[row + 1][col + step] = temp
+    if step >= 3:
+        spin(row + 1, col + 1, step - 2, matrix)
+    return matrix
+
+
+# K 크기의 배열에 저장된 (r, c, s)
+# 순열을 통해서 여러 가지 케이스로 최솟값 내야함
+# spin 함수의 반환값으로 비교해보자
+def compare(cnt, arr):
+    global best
+
+    if cnt == K:
+        matrix = deepcopy(grid)
+        for row, col, step in arr:
+            spin(row - step - 1, col - step - 1, step * 2, matrix)
+        best = min(best, min([sum(j) for j in matrix]))
+
+    for i in range(K):
+        if used[i]:
+            continue
+        used[i] = True
+        arr.append(a[i])
+        compare(cnt + 1, arr)
+        arr.pop()
+        used[i] = False
+
+
 N, M, K = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
+grid = [list(map(int, input().split())) for _ in range(N)]
 
-
-def spin(row, col, space, cnt, t):
-
-    if cnt == 4:
-        if space > 3:
-            spin(row + 1, col + 1, space - 2, 0, 0)
-        else:
-            return
-
-    temp = matrix[row][col + space]
-    for j in range(col + space, col, -1):
-        matrix[row][j] = matrix[row][j - 1]
-    if t:
-        matrix[row][col] = t
-
-    spin(col, row, space, cnt + 1, temp)
-
-
-
-
-
-
-
-
+a = []
 for _ in range(K):
     r, c, s = map(int, input().split())
-    spin(r - s - 1, c - s - 1, s * 2, 0)
+    a.append((r, c, s))
+
+used = [False] * K
+best = float('inf')
+compare(0, [])
+
+print(best)
