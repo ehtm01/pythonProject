78035e290370cf8dcecca786366c7096aee92629
dohyun <ehtm01@gmail.com>
2025-10-29 17:53:04 +0900

    1029_study

--- BAEKJOON/Gold/BAEK_12100_2048_(Easy).py
+++ BAEKJOON/Gold/BAEK_12100_2048_(Easy).py
@@ -7,85 +7,48 @@ N = int(input())
 matrix = [list(map(int, input().split())) for _ in range(N)]
 
 
+def cal(arr):
+    before = [x for x in arr if x]
+    after = []
+    c = 0
+    while c < len(before):
+        if c == len(before) - 1:
+            after.append(before[c])
+            break
+        if before[c] == before[c + 1]:
+            after.append(before[c] * 2)
+            c += 2
+        else:
+            after.append(before[c])
+            c += 1
+
+    after += [0] * (N - len(after))
+    return after
+
+
+# dirs = (0: 상, 1: 하, 2: 좌, 3: 우)
 def moving(dirs, grid):
-    if not dirs % 2:
-        if dirs // 2:
-            for i in range(1, N):
-                for j in range(N):
-                    if grid[i][j]:
-                        if grid[i - 1][j]:
-                            if grid[i - 1][j] == grid[i][j]:
-                                grid[i - 1][j] += grid[i][j]
-                                grid[i][j] = 0
-                        else:
-                            a = i
-                            while a > 0:
-                                if grid[a - 1][j]:
-                                    if grid[a - 1][j] == grid[a][j]:
-                                        grid[a - 1][j] += grid[a][j]
-                                        grid[a][j] = 0
-                                else:
-                                    grid[a - 1][j] = grid[a][j]
-                                    grid[a][j] = 0
-                                a -= 1
-        else:
-            for i in range(N - 2, -1, -1):
-                for j in range(N):
-                    if grid[i][j]:
-                        if grid[i + 1][j]:
-                            if grid[i + 1][j] == grid[i][j]:
-                                grid[i + 1][j] += grid[i][j]
-                                grid[i][j] = 0
-                        else:
-                            a = i
-                            while a < N - 1:
-                                if grid[a + 1][j]:
-                                    if grid[a + 1][j] == grid[a][j]:
-                                        grid[a + 1][j] += grid[a][j]
-                                        grid[a][j] = 0
-                                else:
-                                    grid[a + 1][j] = grid[a][j]
-                                    grid[a][j] = 0
-                                a += 1
+    if dirs == 0:
+        for j in range(N):
+            col = [grid[i][j] for i in range(N)]
+            col = cal(col)
+            for i in range(N):
+                grid[i][j] = col[i]
+
+    elif dirs == 1:
+        for j in range(N):
+            col = [grid[i][j] for i in range(N)]
+            col = cal(col[::-1])[::-1]
+            for i in range(N):
+                grid[i][j] = col[i]
+
+    elif dirs == 2:
+        for i in range(N):
+            grid[i] = cal(grid[i])
+
     else:
-        if dirs // 2:
-            for j in range(1, N):
-                for i in range(N):
-                    if grid[i][j]:
-                        if grid[i][j - 1]:
-                            if grid[i][j - 1] == grid[i][j]:
-                                grid[i][j - 1] += grid[i][j]
-                                grid[i][j] = 0
-                        else:
-                            a = j
-                            while a > 0:
-                                if grid[i][a - 1]:
-                                    if grid[i][a - 1] == grid[i][a]:
-                                        grid[i][a - 1] += grid[i][a]
-                                        grid[i][a] = 0
-                                else:
-                                    grid[i][a - 1] = grid[i][a]
-                                    grid[i][a] = 0
-                                a -= 1
-        else:
-            for j in range(N - 2, -1, -1):
-                for i in range(N):
-                    if grid[i][j]:
-                        if grid[i][j + 1]:
-                            if grid[i][j + 1] == grid[i][j]:
-                                grid[i][j + 1] += grid[i][j]
-                                grid[i][j] = 0
-                        else:
-                            a = j
-                            while a < N - 1:
-                                if grid[i][a + 1]:
-                                    if grid[i][a + 1] == grid[i][a]:
-                                        grid[i][a + 1] += grid[i][a]
-                                        grid[i][a] = 0
-                                else:
-                                    grid[i][a + 1] = grid[i][a]
-                                    grid[i][a] = 0
-                                a += 1
+        for i in range(N):
+            grid[i] = cal(grid[i][::-1])[::-1]
     ans = 0
     for arr in grid:
         ans = max(ans, max(arr))
