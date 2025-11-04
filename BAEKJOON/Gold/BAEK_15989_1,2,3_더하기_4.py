6cf42c43778dc8fa77fa69879e17631f20f041ec
dohyun <ehtm01@gmail.com>
2025-11-04 23:16:48 +0900

    1104_home

--- BAEKJOON/Gold/BAEK_15989_1,2,3_더하기_4.py
+++ BAEKJOON/Gold/BAEK_15989_1,2,3_더하기_4.py
@@ -5,8 +5,15 @@ sys.stdin = open('input.txt', 'r')
 T = int(input())
 for _ in range(T):
     n = int(input())
-    # dp?
-    dp = [0] * n
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 3
+    # dp 생성
+    dp = [0] * (n + 1)
+    # 공집합 개념의 dp[0]
+    dp[0] = 1
+    # 1 부터 3 까지 하니씩 사용
+    for j in range(1, 4):
+        # 루프마다 1, 2, 3 을 추가한 갯수 더해짐
+        for i in range(1, n + 1):
+            if i - j >= 0:
+                dp[i] += dp[i - j]
+
+    print(dp[n])
