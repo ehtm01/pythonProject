9b229e31eb64f85b85c29536775e63eda517d5b0
dohyun <ehtm01@gmail.com>
2025-11-19 20:41:11 +0900

    1119_home

--- BAEKJOON/Gold/BAEK_3190_뱀.py
+++ BAEKJOON/Gold/BAEK_3190_뱀.py
@@ -11,11 +11,9 @@ bodies = deque([(0, 0)])
 
 # 사과 위치
 K = int(input())
-apples = []
 for _ in range(K):
     row, col = map(int, input().split())
-    apples.append((row, col))
-    grid[row][col] = 2  # 사과: 2
+    grid[row - 1][col - 1] = 2  # 사과: 2
 
 # 방향 전환
 L = int(input())
@@ -26,7 +24,6 @@ for _ in range(L):
 
 # 1) 사과를 먹으면 뱀 길이가 늘어남
 # 2) 상하좌우 벽 => 델타
-dirs = (0, 1), (1, 0), (0, -1), (-1, 0)
 # 3) 게임 시작 시 뱀은 맨 위 맨 좌측에 위치, 길이는 1, 방향은 오른쪽
 # 4) 뱀 이동 규칙
 #   1. 몸 길이를 늘려 머리를 다음칸에 위치시킴
@@ -38,7 +35,7 @@ dirs = (0, 1), (1, 0), (0, -1), (-1, 0)
 # 방향 전환 함수
 # R, L 두 가지
 def turn(direction, delta):
-    if direction == 'R':
+    if direction == 'D':
         if delta[0] == 0:
             return delta[1], delta[0]
         else:
@@ -55,12 +52,47 @@ def turn(direction, delta):
 def move_str(y, x, d):
     ny, nx = y + d[0], x + d[1]
     if ny < 0 or ny >= N or nx < 0 or nx >= N:
-        return False
-    if grid[ny][nx] == 1:
-        return False
+        return -1, -1
+    # 사과를 안 먹었을 때는 꼬리 위치를 비워준다.
     if grid[ny][nx] == 0:
         by, bx = bodies.popleft()
         grid[by][bx] = 0
+    # 늦게 확인하는 이유:
+    # 머리가 꼬리를 향할 때 꼬리를 먼저 빼주고 머리가 들어갈 수 있기 때문
+    if grid[ny][nx] == 1:
+        return -1, -1
     bodies.append((ny, nx))
     grid[ny][nx] = 1
-    return True
+    return ny, nx
+
+
+def game():
+    t = 0   # 총 게임 시간
+    r = c = 0   # 최초 위치
+    d = (0, 1)  # 최초 시작 시 오른쪽으로 이동
+    moves_i = 0
+    # 이동 시작
+    while (r, c) != (-1, -1) and moves_i < len(moves):
+        r, c = move_str(r, c, d)
+        t += 1
+        if t == int(moves[moves_i][0]):
+            d = turn(moves[moves_i][1], d)
+            moves_i += 1
+    # for cnt, change in moves:
+    #     cnt = int(cnt)
+    #     loop_cnt = 0
+    #     while loop_cnt < cnt:
+    #         r, c = move_str(r, c, d)
+    #         loop_cnt += 1
+    #         if (r, c) == (-1, -1):
+    #             print(t + loop_cnt)
+    #             return
+    #     t += loop_cnt
+    #     d = turn(change, d)
+    while (r, c) != (-1, -1):
+        r, c = move_str(r, c, d)
+        t += 1
+    print(t)
+
+
+game()
