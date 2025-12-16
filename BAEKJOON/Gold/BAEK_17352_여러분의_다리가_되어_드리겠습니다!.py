0d69ee66c777af8765eb1bcd968f6cef979828d4
dohyun <ehtm01@gmail.com>
2025-12-16 23:53:31 +0900

    1213 BAEK 17352. 빵집 ing

--- BAEKJOON/Gold/BAEK_17352_여러분의_다리가_되어_드리겠습니다!.py
+++ BAEKJOON/Gold/BAEK_17352_여러분의_다리가_되어_드리겠습니다!.py
@@ -1,1 +1,8 @@
 # BAEK 17352. 여러분의 다리가 되어 드리겠습니다!
+import sys
+sys.stdin = open('input', 'r')
+input = sys.stdin.readline
+
+N = int(input())
+for _ in range(N - 2):
+    bridge = list(map(int, input().split()))
