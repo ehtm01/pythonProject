84ac85e01bfcfd92c4279b6efd671a16b8c0a8c5
dohyun <ehtm01@gmail.com>
2026-01-07 20:57:09 +0900

    0107 BAEK 2493 탑 제출

--- BAEKJOON/Gold/BAEK_2493_탑.py
+++ BAEKJOON/Gold/BAEK_2493_탑.py
@@ -3,3 +3,21 @@ import sys
 sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 
+N = int(input())
+tower = list(map(int, input().split()))
+
+stack = []  # (탑의 인덱스, 탑의 높이)
+result = []
+
+for i in range(N):
+    # 현재 탑보다 낮은 탑은 모두 제거
+    while stack and stack[-1][1] < tower[i]:
+        stack.pop()
+    # 스택이 비었다면 왼쪽에 나보다 높은 탑이 없으므로 0
+    if not stack:
+        result.append(0)
+    else:
+        result.append(stack[-1][0] + 1)  # 인덱스는 1-based
+    stack.append((i, tower[i]))
+
+print(' '.join(map(str, result)))
