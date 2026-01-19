f3520e44977d08724a2c665edb26337a59760041
dohyun <ehtm01@gmail.com>
2026-01-19 20:08:07 +0900

    0119 BAEK 24526 전화 돌리기 제출

--- BAEKJOON/Gold/BAEK_24526_전화_돌리기.py
+++ BAEKJOON/Gold/BAEK_24526_전화_돌리기.py
@@ -1,5 +1,5 @@
 # BAEK 24526. 전화 돌리기
-# 노드
+# 위상 정렬, 덱
 import sys
 from collections import deque
 sys.stdin = open('input.txt', 'r')
@@ -10,28 +10,33 @@ input = sys.stdin.readline
 # 같은 부원이 두 번 전화받지 않게 하자.
 
 N, M = map(int, input().split())
-call = [[] for _ in range(N + 1)]
+call = [[] for _ in range(N + 1)]   # 정방향 배열
+reverse_call = [[] for _ in range(N + 1)]   # 역방향 배열
+degree = [0] * (N + 1)  # 진출 차수
+
+# 노드간 정방향/역방향 연결, 차수 증가
 for _ in range(M):
     u, v = map(int, input().split())
     call[u].append(v)
+    reverse_call[v].append(u)
+    degree[u] += 1
 
-call_cnt = 0
+safe = [False] * (N + 1)    # 전화 걸어도 되면 True
+q = deque()
 for i in range(1, N + 1):
-    visited = [False] * (N + 1)
-    q = deque()
-    if call[i]:
-        for j in call[i]:
-            q.append(j)
-        while q:
-            x = q.popleft()
-            if visited[x]:
-                break
-            visited[x] = True
-            for k in call[x]:
-                q.append(k)
-        else:
-            call_cnt += 1
-    else:
-        call_cnt += 1
+    # 차수가 0인 부원은 전화 걸어도 됨
+    # deque에 추가해서 역방향으로 점검
+    if degree[i] == 0:
+        q.append(i)
+        safe[i] = True
 
-print(call_cnt)
+while q:
+    x = q.popleft()
+    # 역방향으로 진행해서 차수가 0이 되면 전화 걸어도 됨
+    for j in reverse_call[x]:
+        degree[j] -= 1
+        if degree[j] == 0:
+            q.append(j)
+            safe[j] = True
+
+print(sum(safe))
